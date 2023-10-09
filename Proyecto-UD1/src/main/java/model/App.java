dc1c3540b3b31e661d963fb06bf81d9a2df7a0d0
DAM2_Alu07 <DAM2_Alu07@smr.ald>
2023-10-09 17:57:58 +0200

    Inicio de proyecto

---
+++ Proyecto-UD1/src/main/java/model/App.java
@@ -0,0 +1,10 @@
+package model;
+
+public class App {
+    private final String filename = "usuarios.bin";
+    private Users users;
+    private Session session;
+
+    public App() {
+    }
+}
---
+++ Proyecto-UD1/src/main/java/model/Main.java
@@ -0,0 +1,17 @@
+package model;
+
+import gui.Login;
+
+public class Main {
+
+	public static void main(String[] args) 	{
+
+		App app = new App();
+		new Login(app);
+
+
+
+
+	}
+
+}
