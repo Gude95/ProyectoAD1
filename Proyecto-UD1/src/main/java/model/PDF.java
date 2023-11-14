c3eb45ab56e1f5d696b331ac0a951aedb416d806
Gude <gude.crujeiras.carlos.ald@gmail.com>
2023-11-14 19:26:07 +0100

    añadida exportación de usuario a pdf

--- Proyecto-UD1/src/main/java/model/PDF.java
+++ Proyecto-UD1/src/main/java/model/PDF.java
@@ -1,24 +1,46 @@
 package model;
 
-import java.io.BufferedWriter;
+
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class PDF {
 
     public void generarPDF (File file, String nombre, String edad, String email){
         System.out.println("pdf");
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
-            bw.write(nombre);
-            bw.newLine();
-            bw.write(edad);
-            bw.newLine();
-            bw.write(email);
+        try{
+            PDDocument documento = new PDDocument();
+            PDPage pagina = new PDPage();
+            documento.addPage(pagina);
+
+            PDPageContentStream contenido = new PDPageContentStream(documento, pagina);
+
+
+            contenido.beginText();
+            contenido.newLineAtOffset(50, 700);
+            contenido.setFont(PDType1Font.HELVETICA_BOLD, 12);
+            contenido.showText("Nombre: " + nombre);
+            contenido.newLineAtOffset(0, -20);
+            contenido.showText("Edad: " + edad);
+            contenido.newLineAtOffset(0, -20);
+            contenido.showText("Correo electronico: " + email);
+            contenido.endText();
+
+
+            contenido.close();
+            documento.save(file);
+            documento.close();
+
+            System.out.println("PDF exportado exitosamente a: " + file);
+
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                e.getMessage();        }
 
     }
 
